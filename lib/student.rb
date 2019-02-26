bc0009b8fce672e0b3cce46661672f91d064fe0e
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-02-26 01:07:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+def initialize
+  @knowledge = []
+end
+
+def learn(lesson)
+  @knowledge.push(lesson)
+end
+
+def knowledge
+  @knowledge
+end
 
 end
